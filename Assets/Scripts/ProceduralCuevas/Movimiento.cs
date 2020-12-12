95749de126d7471f7a2fdb9c4fa02492c9dbfabd
Aique Montes Maestre <UO261072@uniovi.es>
2020-12-12 13:15:48 +0100

    Mas o menos finalizado

---
+++ Assets/Scripts/ProceduralCuevas/Movimiento.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public int playerSpeed;
+    public bool isMoving;
+    void Start()
+    {
+        isMoving = false;
+    }
+
+    void Update()
+    {
+        //Movimiento activado
+        if(isMoving && !Input.GetButton("Fire1")){
+
+            //Obtenemos los valores de la cámara del usuario
+            float valueXAxis = Camera.main.transform.forward.x * Time.deltaTime * playerSpeed;
+            float valueYAxis = Camera.main.transform.forward.y * Time.deltaTime * playerSpeed;
+            float valueZAxis = Camera.main.transform.forward.z * Time.deltaTime * playerSpeed;
+
+            //Movemos al jugador sin cambios en la altura
+            transform.Translate(valueXAxis, valueYAxis, valueZAxis);
+
+        } else if(Input.GetButton("Fire1") && !isMoving){
+            //Activamos movimiento
+            isMoving = true;
+        }else if (Input.GetButton("Fire1") && isMoving){
+            //Desactivamos movimiento
+            isMoving = false;
+        }
+    }
+}
